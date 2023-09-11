5cf0873e4e7bfa426905f58ec4af3a565036b972
Samuel Klein <it@samuel-klein.net>
2023-09-11 18:44:40 +0200

    added ttt class

---
+++ src/main/java/org/github/zimmerlab/TicTacToe.java
@@ -0,0 +1,11 @@
+package org.github.zimmerlab;
+
+import org.github.zimmerlab.basicpackage.BasicBoard;
+
+public class TicTacToe extends BasicBoard implements Game {
+
+	@Override
+	public String gameStatus() {
+		return "not implemented";
+	}
+}
