9075caeac4a1f4299a0186e55e0510904b9895dc
Samuel Klein <it@samuel-klein.net>
2023-09-11 18:46:31 +0200

    added constructor

--- src/main/java/org/github/zimmerlab/TicTacToe.java
+++ src/main/java/org/github/zimmerlab/TicTacToe.java
@@ -4,6 +4,10 @@ import org.github.zimmerlab.basicpackage.BasicBoard;
 
 public class TicTacToe extends BasicBoard implements Game {
 
+	public TicTacToe (int size) {
+
+	}
+
 	@Override
 	public String gameStatus() {
 		return "not implemented";
